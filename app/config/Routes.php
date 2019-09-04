b42900fcb28308b1af8fd04d711612d53b5f4989
disxxn <disonoff@gmail.com>
2019-09-04 16:28:13 +0300

    add routes

---
+++ app/config/Routes.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+  'login' => [
+      'controller' => 'user',
+      'action' => 'auth'
+    ],
+    
+    'users'=>[
+        'controller' => 'user',
+        'action' => 'usersList'
+    ],
+    'user/add' => [
+        'controller' => 'user',
+        'action' => 'add',
+    ],
+    'user/update' => [
+        'controller' => 'user',
+        'action' => 'update',
+    ],
+    'user/delete' => [
+        'controller' => 'user',
+        'action' => 'delete',
+    ],
+
+    'lrs/list' => [
+        'controller' => 'lrs',
+        'action' => 'lrsList'
+    ],
+    'lrs' => [
+        'controller' => 'lrs',
+        'action' => 'lrsShow'
+    ],
+    'lrs/add'  => [
+        'controller' => 'lrs',
+        'action' => 'lrsAdd'
+    ],
+
+    'lrs/client/add' => [
+        'controller' => 'lrsClient',
+        'action' => 'clientAdd'
+    ],
+    'lrs/client/delete' => [
+        'controller' => 'lrsClient',
+        'action' => 'clientDel'
+    ]
+];
