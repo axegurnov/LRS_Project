4d1a09436cfcc834b9a4c64acd24435d26502f9c
disxxn <disonoff@gmail.com>
2019-09-05 15:59:46 +0300

    readme add router, fix router, fix index

--- index.php
+++ index.php
@@ -1,21 +1,12 @@
 <?php
 
 require 'app/config/Dev.php';
-
-use app\core\Model;
+include 'app/config/Autoload.php';
+// use app\core\Model;
 use app\core\Router;
 
 
-
-
-spl_autoload_register(function ($class) {
-    $path = str_replace('\\','/',$class.'.php');
-    if (file_exists($path)){
-        require $path;
-    }
-});
-
-
+    // $asd = new User;
 // $model = new Model();
 // $model->migration();
     $router = new Router;
