b358317d54f3093dd3eb01ccb92cc8210069d038
disxxn <disonoff@gmail.com>
2019-09-05 12:13:07 +0300

    move model

--- app/core/Model.php
+++ app/core/Model.php
@@ -1,33 +1,133 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alex
- * Date: 02.09.19
- * Time: 18:07
- */
 
-namespace app\core\DataBase;
+namespace app\core;
+use app\models\DataBase;
 
-
-
-class Model
+class ParentModel
 {
-    public $db;
+    public $table = null;
+    public $params = array();
+    public $params_changed = array();
 
-    protected $table = '';
-    protected $fieldsInsert = [];
-    protected $errors = [];
+    protected static function buildUpdateSql($data,$table,$id) {
+        $columns = "";
+        $holders = "";
+        foreach ($data as $column => $value) {
+            $columns .= ($columns == "") ? "" : ", ";
+            $columns .= $column."=";
+            $holders .= $holders;
+            $columns .= $value.$holders;
 
-    public function __construct()
-    {
-        $this->db = DataBase::getInstance();
+        }
+        $sql = "UPDATE $table SET $columns WHERE id= $id";
+        return $sql;
+
     }
 
-    public function migration()
-    {
+    protected static function buildInsertSql($data,$table) {
+        $columns = "";
+        $holders = "";
+        foreach ($data as $column => $value) {
+            $columns .= ($columns == "") ? "" : ", ";
+            $columns .= $column;
+            $holders .= ($holders == "") ? "" : ", ";
+            $holders .= $value;
+        }
+        $sql = "INSERT INTO $table ($columns) VALUES ($holders)";
+        return $sql;
+    }
 
-        //debug($this->db->errorDatabase());
+
+    public function getFields()
+    {
+        $fields = array();
+        $sql = "SELECT `COLUMN_NAME` 
+FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+WHERE `TABLE_SCHEMA`='lrs' //Добавить переменную с именем БД
+        AND `TABLE_NAME`= $this->table";
+        $db = DataBase::getInstance();
+
+        $result = $db->query($sql);
+        $rows = mysqli_num_rows($result);
+        $i = 0;
+        $fieldsStr = '';
+        while ($i != $rows) {
+            $row = mysqli_fetch_row($result);
+            $fields[$i] = $row[0];
+                if ($i==0)
+                {
+                    $fieldsStr = "$row[0]";
+                }
+                else
+                {
+                    $fieldsStr = "$fieldsStr".', '."$row[0]";
+                }
+                $i++;
+
+            }
+            $result = array('str'=>$fieldsStr,'array'=>$fields);
+        return $this->params = $result;
+    }
+
+    public function getAllRecords($fields)
+    {
+        $sql = "SELECT $fields FROM $this->table";
+        $db = DataBase::getInstance();
+        $object = $db->query($sql);
+        //$object = mysqli_query($db,$sql);
+        $rows = mysqli_num_rows($object);
+        $i = 0;
+        $allRecords = array();
+        while($i!= $rows){
+            $row = mysqli_fetch_array($object,MYSQLI_ASSOC);
+            $allRecords[$i] = $row;
+            $i++;
+        }
+        return $this->params = $allRecords;
 
     }
+
+    function select($id, $fields = '*')
+    {
+        $sql = "SELECT $fields FROM $this->table WHERE id= $id";
+        $db = DataBase::getInstance();
+        $object = $db->query($sql);
+        return $this->params = mysqli_fetch_array($object);
+
+    }
+
+
+    public function getValue($item)
+    {
+        if(!empty($this->params)){
+            return $this->params[$item];
+        }
+        else {
+            echo 'Нет данных в записи!';
+        }
+    }
+
+    public function setValue($item, $value)
+    {
+        $this->params_changed[$item] = $value;
+        return $this->params[$item] = $value;
+
+    }
+
+    public function updateRecord($id)
+    {
+        $sql = self::buildUpdateSql($this->params_changed,$this->table,$id);
+        $db = DataBase::getInstance();
+        $object = $db->query($sql);
+    }
+
+    public function addRecord()
+    {
+        $sql = self::buildInsertSql($this->params_changed,$this->table);
+        $db = DataBase::getInstance();
+        $object = $db->query($sql);
+
+    }
+
 
 }
