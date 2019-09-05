e2becf8a29c9f02f41c7e38d5c88b09eb10aae56
Alex <alex.egurnov@gmail.com>
2019-09-05 11:16:11 +0300

    add Database

---
+++ app/core/Database.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 05.09.19
+ * Time: 11:12
+ */
+
+namespace app\core;
+use mysqli;
+
+class Database
+{
+    protected $db;
+    private static $_instance = null;
+
+    private function __construct()
+    {
+        $config = require 'app/config/Database.php';
+        $this->db = new mysqli($config['host'], $config['user'], $config['password'],$config['base']) or die ('error');
+    }
+
+    public static function getInstance(){
+        if(self::$_instance === null){
+            self::$_instance = new DataBase();
+        }
+        return self::$_instance;
+    }
+
+    public function query($sql)
+    {
+        return $this->db->query($sql);
+    }
+}
