673c2fa600ddfcaf6c504604949c3fe05890a2be
Anastasiya Hahanova <anastasiya.hahanova@mail.ru>
2019-09-09 16:58:45 +0300

    First Version of Validation

--- app/core/Model.php
+++ app/core/Model.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace app\core;
+
 use app\core\Database;
 
 class Model
@@ -9,7 +10,7 @@ class Model
     public $params = array();
     public $params_changed = array();
     public $db;
-    
+
     private static $_instance = null;
 
     public function __construct()
@@ -17,7 +18,7 @@ class Model
         $this->db = Database::getInstance();
     }
 
-    public static function getInstance($nameModel) 
+    public static function getInstance($nameModel)
     {
         $path = 'app\models\\' . $nameModel;
         if (self::$_instance == null) {
@@ -26,22 +27,22 @@ class Model
         return self::$_instance;
     }
 
-    protected static function buildUpdateSql($data,$table,$id) 
+    protected static function buildUpdateSql($data, $table, $id)
     {
         $columns = "";
         $holders = "";
         foreach ($data as $column => $value) {
             $columns .= ($columns == "") ? "" : ", ";
-            $columns .= $column."=";
+            $columns .= $column . "=";
             $holders .= $holders;
-            $columns .= "'".$value."'".$holders;
+            $columns .= "'" . $value . "'" . $holders;
 
         }
         $sql = "UPDATE $table SET $columns WHERE id= $id";
         return $sql;
     }
 
-    protected static function buildInsertSql($data,$table) 
+    protected static function buildInsertSql($data, $table)
     {
         $columns = "";
         $holders = "";
@@ -49,7 +50,7 @@ class Model
             $columns .= ($columns == "") ? "" : ", ";
             $columns .= $column;
             $holders .= ($holders == "") ? "" : ", ";
-            $holders .= "'".$value."'";
+            $holders .= "'" . $value . "'";
         }
         $sql = "INSERT INTO $table ($columns) VALUES ($holders)";
         return $sql;
@@ -62,7 +63,7 @@ class Model
         $sql = "SELECT `COLUMN_NAME` 
         FROM `INFORMATION_SCHEMA`.`COLUMNS` 
         WHERE `TABLE_SCHEMA`='lrs'
-        AND `TABLE_NAME`= '".$table."';";
+        AND `TABLE_NAME`= '" . $table . "';";
 
         $result = $this->db->query($sql);
         $rows = mysqli_num_rows($result);
@@ -71,15 +72,14 @@ class Model
         while ($i != $rows) {
             $row = mysqli_fetch_row($result);
             $fields[$i] = $row[0];
-            if ($i==0) {
+            if ($i == 0) {
                 $fieldsStr = "$row[0]";
-            }
-            else {
-                $fieldsStr = "$fieldsStr".', '."$row[0]";
+            } else {
+                $fieldsStr = "$fieldsStr" . ', ' . "$row[0]";
             }
             $i++;
         }
-        $result = array('str'=>$fieldsStr,'array'=>$fields);
+        $result = array('str' => $fieldsStr, 'array' => $fields);
         return $this->params = $result;
     }
 
@@ -90,22 +90,21 @@ class Model
         $rows = mysqli_num_rows($object);
         $i = 0;
         $allRecords = array();
-        while($i!= $rows) {
-            $row = mysqli_fetch_array($object,MYSQLI_ASSOC);
+        while ($i != $rows) {
+            $row = mysqli_fetch_array($object, MYSQLI_ASSOC);
             $allRecords[$i] = $row;
             $i++;
         }
         return $this->params = $allRecords;
     }
 
-    function select($predictor,$fields = '*' )
+    function select($predictor, $fields = '*')
     {
         if ($predictor) {
             $sql = "SELECT $fields FROM $this->table WHERE " . $predictor . ";";
             $object = $this->db->query($sql);
             return $this->params = mysqli_fetch_array($object);
-        }
-        else {
+        } else {
             echo "Отсутствует условие!";
         }
     }
@@ -113,12 +112,12 @@ class Model
 
     public function getValue($item)
     {
-        if(!empty($this->params)) {
+        if (!empty($this->params)) {
             return $this->params[$item];
         }
-        else {
-            echo 'Нет данных в записи!';
-        }
+
+        echo 'Нет данных в записи!';
+
     }
 
     public function setValue($item, $value)
@@ -137,13 +136,13 @@ class Model
 
     public function updateRecord($id)
     {
-        $sql = self::buildUpdateSql($this->params_changed,$this->table,$id);
+        $sql = self::buildUpdateSql($this->params_changed, $this->table, $id);
         $object = $this->db->query($sql);
     }
 
     public function addRecord()
     {
-        $sql = self::buildInsertSql($this->params_changed,$this->table);
+        $sql = self::buildInsertSql($this->params_changed, $this->table);
         $object = $this->db->query($sql);
     }
 
@@ -164,5 +163,82 @@ class Model
         $countId = $this->db->query("SELECT COUNT(id) from {$this->table}")->fetch_row();
         return $countId;
     }
+
+    public function Validate($data)
+    {
+        $error = array();
+        $pattern = "/^[а-яА-ЯёЁa-zA-Z]+$/";
+        $email_pattern = "/^[a-z0-9_.\-]+$/i";
+        $desc_pattern = "/^[а-яА-ЯёЁa-zA-Z0-9 ]+$/";
+        function check_length($value = "", $min, $max)
+        {
+            $result = (mb_strlen($value) >= $min && mb_strlen($value) < $max);
+            return $result;
+        }
+
+        if (isset($data['login'])) {
+            $login = !preg_match($email_pattern, trim($data['login']));
+
+            if ($login) {
+                $error['login'] = "Некорректно введен login. Допустимо использовать только латинские буквы, цифры,
+знак подчеркивания («_»), точку («.»), минус («-»)";
+            }
+
+        }
+
+        if (isset($data['password'])) {
+            $min = 8;
+            $max = 25;
+            if (!check_length(trim($data['password']), $min, $max)) {
+                $error['password'] = "Пароль должен содержать от восьми до двадцати пяти символов!";
+            }
+
+        }
+        if (isset($data['name'])) {
+            $name = !preg_match($pattern, trim($data['name']));
+            if ($name) {
+                $error['name'] = "Имя введено неверно. Допустимо использвать только латинские буквы и/или кириллицу";
+            }
+            $num_words = explode(" ", trim($data['name']));
+            if (sizeof($num_words) > 1)
+            {
+                $error['name'] = $error['name']." Имя должно состоять из одного слова";
+            }
+
+        }
+        if (isset($data['second_name'])) {
+            $name = !preg_match($pattern, trim($data['name']));
+            if ($name) {
+                $error['name'] = "Фамилия введена неверно. Допустимо использвать только латинские буквы и/или кириллицу";
+            }
+
+        }
+
+        if (isset($data['email'])) {
+            if (strpbrk("@", $data['email'])) {
+                $partsOfEmail = explode("@", trim($data['email']));
+                $partOne = !preg_match($email_pattern, $partsOfEmail[0]);
+                $partTwo = !preg_match($email_pattern, $partsOfEmail[1]);
+
+                if (($partOne) || ($partTwo)) {
+                    $error['email'] = "Некорректно введен email. Допустимо использовать только латинские буквы, цифры,
+знак подчеркивания («_»), точку («.»), минус («-»)";
+                }
+            } else
+            {
+                $error['email'] = "Email должен содержать знак : @ ";
+            }
+        }
+
+        if (isset($data['description'])) {
+            $description = !preg_match($desc_pattern, trim($data['description']));
+            if ($description) {
+                $error['description'] = "Имя должно состоять из трех слов";
+            }
+        }
+
+        return $error;
+    }
+
 
 }
