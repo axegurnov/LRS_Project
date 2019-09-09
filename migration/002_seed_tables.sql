f5c76925d55bf8944562ae309c206d97ac35b10d
alex <alex.egurnov@gmail.com>
2019-09-09 12:11:52 +0300

    update seed

--- migration/002_seed_tables.sql
+++ migration/002_seed_tables.sql
@@ -1,10 +1,14 @@
 insert into users (login, password, name, email) value ('admin','$2y$10$a2ZFoaZVvDMyuaLJmG2WDuNz./vXAv6HUyvfytnxTNqDOCd0beJD2','alex','google@gmail.com');
 insert into users (login, password, name, email) value ('adm2in2','$2y$10$A9Qoa.ISE.JiKmYak03NGOxuKAqdUeJE61rJYxZ2mK49sKXvhOvFS','neron','gasd@gmail.com');
 
-insert into lrs (name, description) value ('Course 1','about course 1');
-insert into lrs (name, description) value ('Course 2','about course 2');
-insert into lrs (name, description) value ('Course 3','about course 3');
-insert into lrs (name, description) value ('Course 4','about course 4');
+insert into lrs (name, description) value ('LRS 1','about LRS 1');
+insert into lrs (name, description) value ('LRS 2','about LRS 2');
+insert into lrs (name, description) value ('LRS 3','about LRS 3');
+insert into lrs (name, description) value ('LRS 4','about LRS 4');
+insert into lrs (name, description) value ('LRS 5','about LRS 5');
+insert into lrs (name, description) value ('LRS 6','about LRS 6');
+insert into lrs (name, description) value ('LRS 7','about LRS 7');
+insert into lrs (name, description) value ('LRS 8','about LRS 8');
 
 insert into lrs_client (lrs_id, login, password, description) value ('4','vasya','123','Vasya');
 insert into lrs_client (lrs_id, login, password, description) value ('3','petya','123','Petya');
