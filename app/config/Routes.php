39850f6cef406bf9d03b6ef846a1649665c46dae
disxxn <disonoff@gmail.com>
2019-09-10 15:42:52 +0300

    fix route

--- app/config/Routes.php
+++ app/config/Routes.php
@@ -6,8 +6,8 @@ class Routes
     {
         return [
             '' => [
-              'controller' => 'checkRoutes',
-              'action' => 'checkRoutes',
+              'controller' => 'user',
+              'action' => 'auth',
             ],
 
             'login' => [
