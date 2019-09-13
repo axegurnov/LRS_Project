2d1fa846042dc2c7967ba13d0be0cb5e6e6cfaf2
Anastasiya Hahanova <anastasiya.hahanova@mail.ru>
2019-09-13 17:12:07 +0300

    Validation and index.php

--- app/core/Validation.php
+++ app/core/Validation.php
@@ -3,55 +3,30 @@
 namespace app\core;
 class Validation
 {
-    public static function checkLength($value = "", $min, $max)
-    {
-        $result = (strlen(trim($value)) >= $min && strlen(trim($value)) < $max);
-        return $result;
-    }
-
     public static function Validate($data)
     {
+      $fail = false;
       $error = array();
       $config = require 'app/config/configValidate.php';
 
     foreach ($data as $key => $value) {
         $value = trim($value);
-        if ((isset($config['empty'][$key]))||(!isset($config['patterns'][$key])))
+        if (isset($config['patterns'][$key]))
         {
-            unset($data[$key]);
-        }
-
-        if ((empty($value))&&(!isset($config['empty'][$key]))) {
+            if (empty($value)) {
                 $error[$key] = "Значение $key не введено!";
             }
-    }
-
-        if ($error)
-        {
-            return $_SESSION['errors'] = $error;
-        }
-        else
-        {
-            foreach ($data as $key => $value) {
-                $value = trim($value);
-
-                if ($key == "password"){
-                $min = 8;
-                $max = 25;
-                $fail = !self::checkLength($value,$min,$max);
-                }
-
-                if(isset($config['patterns'][$key])){
-                $pattern = $config['patterns'][$key];
-                $fail = !preg_match($pattern, trim($data[$key]));
-
-                }
+            else {
+              $pattern = $config['patterns'][$key];
+              $fail = !preg_match($pattern, trim($data[$key]));
+            }
 
         if ($fail){
-            $error[$key] = $config['messages'][$key];  }
+            $error[$key] = $config['messages'][$key];
         }
-          return $_SESSION['errors'] = $error;
         }
-    }
+      }
 
+          return $_SESSION['errors'] = $error;
+   }
 }
