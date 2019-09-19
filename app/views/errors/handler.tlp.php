f5c73651b920683b254002a1cb9343fcf447a9f4
Naruto12346 <roma_naruto@mail.ru>
2019-09-19 17:25:31 +0300

    Fix layout handler

--- app/views/errors/handler.tlp.php
+++ app/views/errors/handler.tlp.php
@@ -1,13 +1,4 @@
-<?php ob_start(); ?>
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?=(!isset($title)) ? '' : $title?></title>
-    <link rel="stylesheet" href="/css/bootstrap.css">
-</head>
+<?php require '../app/views/layouts/header.tlp.php'; ?>
     <body>
         <div class="wrapper">
             <main>
