f1d7a61d7e2b3d9f7ae8bee20a0d35da33d7e83d
disxxn <disonoff@gmail.com>
2019-09-19 15:38:31 +0300

    statements in json

--- app/views/lrs/statements.tlp.php
+++ app/views/lrs/statements.tlp.php
@@ -12,30 +12,6 @@
                 <a class="nav-item nav-link active" href="<?= route('lrs_statements'); ?>?lrs=<?= $lrs['id'] ?>" role="tab" aria-controls="nav-contact" aria-selected="false">Statements</a>
             </div>
         </nav>
-        <!-- Большие модальное окно -->
-
-        <div class="modal fade modal-all-statements">
-            <div class="modal-dialog modal-lg">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">Statements in JSON</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <pre><?php
-                            echo json_encode($statementsJson, JSON_PRETTY_PRINT)
-                            /*foreach($statementsJson as $sta) {
-                                    echo json_encode($sta, JSON_PRETTY_PRINT);
-                                    echo "<BR>";
-                                }*/
-                            ?>
-                        </pre>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="tab-content" id="nav-tabContent">
             <div class="tab-pane fade show active" id="nav-home" role="tabpanel">
                 <div class="row">
@@ -48,22 +24,21 @@
                                 <th scope="col">Verb</th>
                                 <th scope="col">Activity</th>
                                 <th scope="col">Content</th>
-                                <th><button class="btn btn-light float-right" data-toggle="modal" data-target=".modal-all-statements">JSON</button></th>
+                                <th></th>
                             </tr>
                             </thead>
                             <tbody>
                             <?php $i=1; foreach ($statements as $statement): ?>
                                 <tr>
                                     <th scope="row"><label><?= $i?></label></th>
-                                    <td><?= $statement['login'] ?></td>
-                                    <td><?= $statement['verb'] ?></td>
-                                    <td><?= $statement['activity'] ?></td>
-                                    <td><?= $statement['content'] ?></td>
+                                    <td id="statementLogin" class="statementInfo" statementId="<?=$i?>"><?= $statement['login'] ?></td>
+                                    <td id="statementVerb" class="statementInfo" statementId="<?=$i?>"><?= $statement['verb'] ?></td>
+                                    <td id="statementActivity" class="statementInfo" statementId="<?=$i?>"><?= $statement['activity'] ?></td>
+                                    <td id="statementContent" class="statementInfo" statementId="<?=$i?>"><?= $statement['content'] ?></td>
                                     <td>
                                         <button id="btnStatementId" class="btn btn-info float-right showStatementInJson" btnStatementid="<?=$i?>" data-toggle="modal" data-target=".modal-single-statement">JSON</button>
                                         <div id="statementId" class="statementInJson" divStatementId="<?=$i?>" hidden="hidden">
                                             <pre><?php
-
                                                     echo json_encode($statement, JSON_PRETTY_PRINT);
                                                 ?>
                                             </pre>
--- public/js/showStatementJson.js
+++ public/js/showStatementJson.js
@@ -1,3 +1,24 @@
+/* $(document).ready(function() {
+    let statements = $(".statementInfo");
+    let statementBtn = $(".showStatementInJson");
+
+    $(statementBtn).click(function (e) {
+        let statementInfo = [];
+        let btnId = e.target.getAttribute('btnStatementid');
+        for(let i = 0; i < statements.length; i++) {
+            if(statements[i].getAttribute('statementId') === btnId) {
+                if(statements[i].getAttribute('id') === 'statementLogin') {
+                    console.log(statements[i].getAttribute('id'));
+                    statementInfo.push(statements[i].innerText);
+                }
+            }
+        }
+
+        console.log(statementInfo);
+    });
+}); */
+
+
 $(document).ready(function() {
     let statements = $(".statementInJson");
     let statementBtn = $(".showStatementInJson");
