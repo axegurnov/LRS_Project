fd78cbf13fa99a4f5a0d4acfc6ce239bcd8789e3
Naruto12346 <roma_naruto@mail.ru>
2019-09-23 13:00:47 +0300

    For nastya

--- migration/data/004_fill_table_lrs_state.sql
+++ migration/data/004_fill_table_lrs_state.sql
@@ -1,3 +1,3 @@
-INSERT INTO lrs_state (lrs_id, lrs_client_id, state_key, value, activity, registration) VALUE ('2','4','432dfqw','var1','video','200');
-INSERT INTO lrs_state (lrs_id, lrs_client_id, state_key, value, activity, registration) VALUE ('1','1','43h465','var3','text','200');
-INSERT INTO lrs_state (lrs_id, lrs_client_id, state_key, value, activity, registration) VALUE ('3','3','432dfqw','var1','site','200');
+INSERT INTO lrs_state (lrs_id, lrs_client_id, state_key, value, activity_id, registration) VALUE ('2','4','432dfqw','var1','2','200');
+INSERT INTO lrs_state (lrs_id, lrs_client_id, state_key, value, activity_id, registration) VALUE ('1','1','43h465','var3','1','200');
+INSERT INTO lrs_state (lrs_id, lrs_client_id, state_key, value, activity_id, registration) VALUE ('3','3','432dfqw','var1','1','200');
---
+++ migration/edit/004_personal_edit.sql
@@ -0,0 +1,9 @@
+ALTER TABLE lrs.lrs_state
+CHANGE COLUMN activity activity_id INT(200) NOT NULL ,
+ADD INDEX fk_lrs_state_1_idx (activity_id ASC);
+ALTER TABLE lrs.lrs_state
+ADD CONSTRAINT lrs_state_ibfk_3
+  FOREIGN KEY (activity_id)
+  REFERENCES lrs.activity (id)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
