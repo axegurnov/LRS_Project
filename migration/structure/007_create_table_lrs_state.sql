6a56a791016cf152311596f077d1ce1aab864bbf
Naruto12346 <roma_naruto@mail.ru>
2019-09-23 14:06:32 +0300

    awes

--- migration/structure/007_create_table_lrs_state.sql
+++ migration/structure/007_create_table_lrs_state.sql
@@ -4,9 +4,10 @@ create table if not exists lrs_state (
     lrs_client_id int (100) not null,
     state_key varchar (100) not null,
     value varchar (100) not null,
-    activity varchar (100) not null,
+    activity_id int (100) not null,
     registration varchar (100) not null,
     primary key (id),
     FOREIGN KEY (lrs_id) REFERENCES lrs (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (lrs_client_id) REFERENCES lrs_client (id) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (lrs_client_id) REFERENCES lrs_client (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (activity_id) REFERENCES activity (id) ON UPDATE CASCADE ON DELETE CASCADE,
 );
